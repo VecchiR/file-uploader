5bf2fc985327dfcb076c06c88c2e7686ad2adafd
Vecchi <79600745+VecchiR@users.noreply.github.com>
2025-06-04 09:34:13 -0300

    update app.js to use dynamic port

--- app.js
+++ app.js
@@ -49,4 +49,5 @@ app.set('view engine', 'ejs');
 
 app.use(routes);
 
-app.listen(process.env.PORT, () => console.log('app listening on port: ', process.env.PORT));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('app listening on port: ', port));
