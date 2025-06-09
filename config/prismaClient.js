2c2098eab35b54d61c9bd74569d2dce24f76f0da
Vecchi <79600745+VecchiR@users.noreply.github.com>
2025-06-09 15:18:29 -0300

    Prisma models: create Folder and File, update User

--- config/prismaClient.js
+++ config/prismaClient.js
@@ -3,5 +3,7 @@ const { PrismaClient } = require('../generated/prisma');
 const prismaClient = new PrismaClient();
 
 const User = prismaClient.user;
+const Folder = prismaClient.folder;
+const File = prismaClient.file;
 
-module.exports = { prismaClient, User }
+module.exports = { prismaClient, User, Folder, File }
---
+++ prisma/migrations/20250609154932_folder/migration.sql
@@ -0,0 +1,47 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT 'New Folder',
+    "ownerId" TEXT NOT NULL,
+    "parentFolderId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT 'New File',
+    "ownerId" TEXT NOT NULL,
+    "parentFolderID" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentFolderId_fkey" FOREIGN KEY ("parentFolderId") REFERENCES "Folder"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_parentFolderID_fkey" FOREIGN KEY ("parentFolderID") REFERENCES "Folder"("id") ON DELETE SET NULL ON UPDATE CASCADE;
